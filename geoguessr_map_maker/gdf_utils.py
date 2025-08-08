6340991826cf9a074e0b64d70a35f3f06f2de7d2
Megan Leet <megan.leet@outlook.com.au>
2025-08-08 23:14:06 +1000

    Spruce up read_geo_file a bit, since I find myself using it a lot

--- geoguessr_map_maker/gdf_utils.py
+++ geoguessr_map_maker/gdf_utils.py
@@ -1,7 +1,7 @@
 import asyncio
 from collections.abc import Iterable
 from pathlib import Path
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING, Any, cast
 
 import geopandas
 import numpy
@@ -9,21 +9,39 @@ import pandas
 from tqdm.auto import tqdm
 
 if TYPE_CHECKING:
-	from shapely import Point
+	from shapely import Geometry, Point
 
 
-def read_geo_file(path: Path) -> geopandas.GeoDataFrame:
+def read_geo_file(
+	path: Path | str, crs: Any = None, mask: 'Geometry | None' = None
+) -> geopandas.GeoDataFrame:
+	"""Reads a file with geopandas, showing a progress bar.
+
+	Arguments:
+		crs: If set, converts the GeoDataFrame to this CRS after reading.
+		mask: If set, only loads rows inside a geometry as with the mask parameter to geopandas.read_file.
+	
+	Raises:
+		TypeError: If reading the file resulted in something other than a GeoDataFrame.
+	"""
+	if isinstance(path, str):
+		path = Path(path)
+
 	with (
 		path.open('rb') as f,
 		tqdm.wrapattr(f, 'read', total=path.stat().st_size, desc=f'Reading {path}') as t,
 	):
-		gdf = geopandas.read_file(t)
+		gdf = geopandas.read_file(t, mask)
 		if not isinstance(gdf, geopandas.GeoDataFrame):
 			raise TypeError(f'{path} contains {type(gdf)}, expected GeoDataFrame')
+		if crs is not None:
+			gdf = gdf.to_crs(crs)
 		return gdf
 
-async def read_geo_file_async(path: Path) -> geopandas.GeoDataFrame:
-	return await asyncio.to_thread(read_geo_file, path)
+
+async def read_geo_file_async(path: Path | str, crs: Any = None, mask: 'Geometry | None' = None) -> geopandas.GeoDataFrame:
+	return await asyncio.to_thread(read_geo_file, path, crs, mask)
+
 
 def count_points_in_each_region(
 	points: Iterable['Point'], regions: geopandas.GeoDataFrame, name_col: str
